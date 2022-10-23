6dca2c89bc64219cd49718d5770d13eb0fc61527
Mateusz <mat.bereda@gmail.com>
2022-10-23 11:09:17 +0200

    budowniczy

--- src/pl/camp/it/car/rent/Main.java
+++ src/pl/camp/it/car/rent/Main.java
@@ -1,6 +1,9 @@
 package pl.camp.it.car.rent;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import pl.camp.it.car.rent.model.Car;
+import pl.camp.it.car.rent.model.Motorcycle;
+import pl.camp.it.car.rent.model.builders.MotorcycleBuilder;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,5 +35,29 @@ public class Main {
 
         y = x==5 ? true : false;
         z = x==5 ? "ABC" : "XYZ";
+
+        String s1 = "ABC";
+        String s2 = "XYZ";
+
+        String result = s1 + s2;
+        System.out.println(result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(s1).append(s2).append("asdf").append("sadhkjfg").append("asdf");
+        result = sb.toString();
+        System.out.println(result);
+
+        Car car = new Car();
+        //car.setModel("asdf").setBrand("akhsdgkj");
+
+
+        Motorcycle motorcycle = new MotorcycleBuilder()
+                .brand("Audi").model("A6").year(2022)
+                .plate("KR99").price(234.34).cart(true).build();
+
+        System.out.println(motorcycle.getBrand());
+        System.out.println(motorcycle.getModel());
+        System.out.println(motorcycle.getPlate());
+        System.out.println(motorcycle.isCart());
     }
 }
